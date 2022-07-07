42b8750ccc9c277bc7d4fb8801786dc3fbfea68b
Adam <adam.l.sosa@gmail.com>
2022-07-07 17:13:34 -0400

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// Write your code in this file!
-const currentUser = 'Pickle Rick';
-const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ' + ${currentUser}!`;
+
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
